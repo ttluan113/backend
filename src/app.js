fccf80da650ba081d9a78c02e84aaabaf5306580
ttluan113 <ttluan113@gmail.com>
2023-12-30 18:33:09 +0700

    123

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ db.Connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
-<<<<<<< HEAD
 app.use(cors({origin: 'https://fullstacknew.vercel.app/'}));
-=======
-app.use(cors({origin: 'https://fullstack-henna.vercel.app'}));
->>>>>>> 230cac3872260ab7e88ef992449bcc731d45612c
 
 route(app);
 
